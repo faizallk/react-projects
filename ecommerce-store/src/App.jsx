ac28b1e5c152d10924907bc716d1d67805618750
faizallk <khanfaijal699@gmail.com>
2025-06-02 13:03:25 +0530

    modifying styling of App.jsx and Home.jsx

--- ecommerce-store/src/App.jsx
+++ ecommerce-store/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Home from './pages/Home'
+import Details from './pages/Details'
 import {Routes,Route} from 'react-router-dom'
 function App() {
   return (
-    <div className='h-screen w-screen bg-red-200 flex'>
+    <div className='h-screen w-screen flex'>
     <Routes>
       <Route path="/" element={<Home />} />
-      {/* <Route path="/details/:id" element={<Details />} /> */}
+      <Route path="/details/:id" element={<Details />} />
     </Routes>
     </div>
   )
--- ecommerce-store/src/pages/Home.jsx
+++ ecommerce-store/src/pages/Home.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import Nav from '../Components/Nav'
 import Card from '../Components/Card'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
     <>
     <Nav />
-   <div className='w-[85%] h-screen bg-blue-200 flex flex-wrap overfow-x-hidden overflow-y-auto p-10 pt-[5%]'>
+   <Link to="/details/1" className='w-[85%] h-screen bg-white flex flex-wrap overfow-x-hidden overflow-y-auto p-10 pt-[5%]'>
    <Card />   
-   </div>
+   </Link>
     
     </>
   )
