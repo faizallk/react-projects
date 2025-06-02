64fc7a0effd8b285dc3e4f640180b567410c5896
faizallk <khanfaijal699@gmail.com>
2025-06-02 14:51:07 +0530

    Modifying Home.jsx,Details.jsx and main.jsx

--- ecommerce-store/src/pages/Details.jsx
+++ ecommerce-store/src/pages/Details.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import {ProductContext} from '../utils/Context'
 
 const Details = () => {
+ const {id} = useParams();
+ const [products] = useContext(ProductContext);
+ const {title,image,description,price,category} = products[id-1];
+
   return (
     <div className="flex justify-center items-center max-w-4xl mx-auto mt-5 mb-5 bg-white rounded-2xl shadow-lg overflow-hidden border border-gray-200 p-5">
       <div className="w-1/2 h-[100%]">
         <img
-          src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+          src={`${image}`}
           alt=""
           className="w-full h-full object-contain"
         />
@@ -13,17 +19,17 @@ const Details = () => {
       <div className="w-1/2 p-13 flex flex-col">
         <div>
           <h2 className="text-3xl font-bold text-gray-800 mb-3">
-            Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            {title}
           </h2>
           <p className="text-gray-600 mb-4">
-            Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday
+            {description}
           </p>
         </div>
 
-        <div className="text-2xl font-semibold text-green-600 mb-4">$109.95</div>
+        <div className="text-2xl font-semibold text-green-600 mb-4">${price}</div>
 
         <div className=" font-semibold text-zinc-400 mb-5">
-          Category: Men's Clothing
+          {category}
         </div>
 
         <div className="flex gap-3">
--- ecommerce-store/src/pages/Home.jsx
+++ ecommerce-store/src/pages/Home.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Nav from '../Components/Nav'
-import Card from '../Components/Card'
 import { Link } from 'react-router-dom'
-
+import { ProductContext } from '../utils/Context';
+import Loading from '../Components/Loading';
 function Home() {
-  return (
+   const [products] = useContext(ProductContext);
+  return products? (
     <>
     <Nav />
-   <Link to="/details/1" className='w-[85%] h-screen bg-white flex flex-wrap overfow-x-hidden overflow-y-auto p-10 pt-[5%]'>
-   <Card />   
+   
+    <div  className='w-[85%] h-screen bg-white flex flex-wrap overfow-x-hidden overflow-y-auto p-10 pt-[5%]'>
+     {products.map((p,i)=>(
+        <Link key={i} to={`/details/${p.id}`} className='m-auto shadow border rounded border-zinc-300 mb-5 card w-50 h-55 text-ellipsis overflow-hidden bg-white py-5 px-2 text-center hover:shadow-zinc-400 duration-200'>
+      <div className='h-[80%] w-full bg-contain bg-no-repeat bg-center mb-1 hover:scale-115 hover:ease-out duration-200 ' style={{
+        backgroundImage:`url(${p.image})`
+      }}></div>
+      <h1 className='hover:text-blue-300 hover:cursor-pointer duration-100'>{p.title}</h1>
+  
    </Link>
-    
+     ))}
+     </div>
     </>
-  )
+  ):(<Loading />)
 }
 
 export default Home
