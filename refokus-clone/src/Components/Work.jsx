1e8f6f123e75c108b539c384b77dbb6f80599933
faizallk <khanfaijal699@gmail.com>
2025-06-11 16:26:51 +0530

    reinstall react-icon and tailwindcss

--- refokus-clone/src/Components/Work.jsx
+++ refokus-clone/src/Components/Work.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Work() {
   const  images = [
-        {url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef09178195ce0073e38f3_Refokus%20Tools-1.png" , top: "50%", left: "50%", isActive: true},
+        {url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef09178195ce0073e38f3_Refokus%20Tools-1.png" , top: "50%", left: "50%", isActive: false},
         {url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0accfe1b3e66bc55462_Refokus%20Tools.png" , top: "56%", left: "44%", isActive: false},
         {url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0acbc45cb2f4fc5c6b2_Yahoo.png" , top: "45%", left: "56%", isActive: false},
         {url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef092455ce2cf591e52d1_Rainfall.png" , top: "60%", left: "53%", isActive: false},
