5234b6a4a71ff74fbb44ae890b6110eb7539fd19
faizallk <khanfaijal699@gmail.com>
2025-06-12 11:38:59 +0530

    Changing th hard coded data from Button.jsx to dynamic

--- refokus-clone/src/App.jsx
+++ refokus-clone/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './Components/Navbar'
 import Work from './Components/Work'
 import Stripes from './Components/Stripes'
+import Products from './Components/Products'
 function App() {
   return (
     <div className='w-full h-screen bg-zinc-900 text-white font-["Sutoshi Variable"]'>
       <Navbar />
       <Work />
       <Stripes />
+      <Products />
     </div>
   )
 }
